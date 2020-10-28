c409e403514e2412f352e031b666aa1416a80269
Maksym Kosenko <maksym.kosenko@biko2.com>
2020-10-28 17:01:50 +0100

    Fix >> jest, Add >> enzyme, enzyme-adapter-react-17, react-test-renderer, node-sass; >> test :zany_face:

---
+++ src/pages/product-list-page.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ProductListPageContainer } from "./product-list-page-container.styles";
+
+const ProductListPage = () => (
+  <ProductListPageContainer>
+
+  </ProductListPageContainer>
+)
+
+export default ProductListPage;
---
+++ src/tests/product-list-page.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {shallow} from "enzyme";
+import ProductListPage from "../pages/product-list-page.component";
+
+
+it('should render PLP - Product List Page', () => {
+  expect(shallow(<ProductListPage />)).toMatchSnapshot();
+});
